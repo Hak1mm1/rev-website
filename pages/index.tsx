527f3e00d9899e7f33c9170ef217d8ac1e3b396d
Hak1mm1 <abdulhakimhussein29@gmail.com>
2025-04-16 12:32:03 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Your Revision Website</h1>
+      <p>This is a simple platform to help you revise all your subjects!</p>
+    </div>
+  );
+};
+
+export default HomePage;
